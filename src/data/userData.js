a76ffaeb485538029f81ea7f94b5999ea4051de9
doyoung <sevim0104@naver.com>
2022-12-20 20:48:38 +0900

    Update 업데이트 오류 문제 해결

--- src/data/userData.js
+++ src/data/userData.js
@@ -3,7 +3,12 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  // 보안상 비공개  입니다.
+  apiKey: "AIzaSyA1YL185M8lRdJuKAmWogX1TsO073Tily4",
+  authDomain: "with-react-a047a.firebaseapp.com",
+  projectId: "with-react-a047a",
+  storageBucket: "with-react-a047a.appspot.com",
+  messagingSenderId: "342844357085",
+  appId: "1:342844357085:web:cad66f8621e6ae1a711e34",
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -28,17 +33,3 @@ export async function dataLoad() {
 }
 
 export const storage = firebase.storage();
-
-// 로그인 정보
-
-export let userInpo;
-
-// firebase.auth().onAuthStateChanged((user) => {
-//   if (user) {
-//     userInpo = {
-//       displayName: user.displayName,
-//       uid: user.uid,
-//     };
-//     console.log(userInpo);
-//   }
-// });
